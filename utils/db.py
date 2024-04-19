13b1e3928ca9d98b0f5ecea265188715112b70c3
SudoingSudori <SudoingSudori@proton.me>
2024-04-19 14:59:57 +0300

    [utils/db] some db work

--- utils/db.py
+++ utils/db.py
@@ -7,31 +7,48 @@ Reminders
     start           When was added (in seconds Unix epoch, used also to calculate next repeat date)
     isrepeatable    True if the reminder is, obviously, repeatable
     repeatcount     Amount of repeats already done (to calculate next repeat)
-    end             When to remind (in seconds)
+    end             When to remind (offset, in seconds)
 """
 
 import aiosqlite
+import asyncio
+import time
 from typing import Optional
 
-async def db_init(db: aiosqlite.Connection):
-    await db.execute("CREATE TABLE IF NOT EXISTS reminders(name TEXT, description TEXT, start INTEGER,"
-                     " isrepeatable INTEGER, repeatcount INTEGER, end INTEGER);")
-    await db.commit()
-    return
 
-async def add_reminder(db: aiosqlite.Connection, name: str, description: str, end: int, isrepeatable: Optional[bool]):
-    """
-    Adds a reminder.
-    Notes: start is counted from the moment of the function executing, repeatcount is default to 1 and increased separately
-    """
+class ReminderDB():
 
-async def remove_reminder(db: aiosqlite.Connection, name: str):
-    """
-    Removes the reminder by name.
-    """
+    def __init__(self, db: aiosqlite.Connection):
+        self.db = db
+    
+    async def db_init(self):
+        await self.db.execute("CREATE TABLE IF NOT EXISTS reminders(name TEXT, description TEXT, start INTEGER,"
+                        " isrepeatable INTEGER, repeatcount INTEGER, end INTEGER);")
+        await self.db.commit()
 
-async def update_reminder(db: aiosqlite.Connection, name: str, update_data: dict):
-    """
-    Updates the reminder data.
-    Notes: Primarily just to update the repeat counter, but could be used for other uses (updating description etc).
-    """
+    async def add(self, name: str, description: str, end: int, isrepeatable: Optional[bool] = False):
+        """
+        Adds a reminder.
+        Notes: start is counted from the moment of the function executing, repeatcount is default to 0 and increased separately
+        """
+        start = time.time()
+        isrepeatable = int(isrepeatable)
+        repeatcount = 0
+        await self.db.execute("INSERT INTO reminders(name, description, start, isrepeatable, repeatcount, end) VALUES (?, ?, ?, ?, ?, ?);", 
+                        (name, description, start, isrepeatable, repeatcount, end))
+        await self.db.commit()
+        return
+
+    async def remove(self, name: str):
+        """
+        Removes the reminder by name.
+        """
+        await self.db.execute("DELETE FROM reminders WHERE name = ?;", (name,))
+        await self.db.commit()
+        return
+
+    async def update(self, name: str, update_data: dict):
+        """
+        Updates the reminder data.
+        Notes: Primarily just to update the repeat counter, but could be used for other uses (updating description etc).
+        """
